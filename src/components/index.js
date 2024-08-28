5e5251474f73338488e70e469a8130f0f31287d4
pankaj <146415250+itzpankuu@users.noreply.github.com>
2024-08-28 19:31:47 +0530

    commit
    commit

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import TodoForm from "./TodoForm";
+import TodoItem from "./TodoItem";
+import { useTodo, TodoProvider } from "../context/TodoContex";
+
+export {TodoForm ,TodoItem};
+export {useTodo, TodoContext, TodoProvider } from '../context/TodoContex'
